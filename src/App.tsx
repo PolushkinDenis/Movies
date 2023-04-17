9d0ff6d22c8712e24511b56bc348aba6fa113139
Grid-Master <you@example.com>
2023-04-17 20:08:08 +0300

    test commit 2

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './scss/app.scss';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import Header from './components/header/Header';
+import ContentPage from './pages/contentPage/ContentPage';
+import Main from './pages/main/Main';
+//import Films from "./pages/films/Films";
+import Footer from './components/footer/Footer';
+import Movies from './pages/movies/Movies';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="/" element={<Main />} />
+          {/* <Route path="/films" element={<Films />} /> */}
+          <Route path="/film" element={<ContentPage />} />
+          <Route path="/movies" element={<Movies />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
